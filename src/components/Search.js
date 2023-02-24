a1957ac5eef537f5a8c110aa045b22dbb4a9034b
WrightChristian-FS <83625690+WrightChristian-FS@users.noreply.github.com>
2023-02-23 17:49:11 -0800

    Modified code to include functionality to properly pass data to the app.js using states and onchange

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import Button from './Button'
+import   {useState} from 'react';
+function Search(props){
 
-const Search = props => {
+    // Set the title and use state 
+    const [title,setTitle] = useState('');
+
+    // The function will instantite on a change 
+    const handleChange =(event)=>{
+        setTitle(event.target.value);
+    };
+
+    // When there is a submission (enter key or search button) prevent the default and pass the input data to the onCreate function 
+    const handleSubmit =(event)=>{
+     event.preventDefault();
+       props.onCreate(title);  
+    };
     return (
         <div className='search' style={styles.container}>
 
-            <h2>Employee Search</h2> 
+            <h2 style={styles.header}>Employee Search</h2> 
             <p style={styles.instructions}>Enter the Countries Abbriviation code : AU, BR, CA, GB, MX, US</p>
 
             <form
-                style={styles.section}>
-              
-                {/* Input to get the country code requested by the user. This is explicitly limited to 2 characters  */}
-                <input placeholder='US' value={props.employeeName} style={styles.input} maxLength='2' ></input>
-                <Button buttonTitle="Search" buttonStyle={styles.button} onSubmit={props.Test}/>
-            </form>
+                style={styles.section}
+                 onSubmit={handleSubmit}>
+
+                <input onChange={handleChange} placeholder='US' value={title} style={styles.input} maxLength='2' ></input>
+                <Button buttonTitle="Search" buttonStyle={styles.button}/>
+            </form> 
         </div>
     )
 }
@@ -23,20 +37,16 @@ export default Search
 
 const styles = {
     container: {
-        // backgroundColor: '#F0F4EFF0F4EF',
         padding: '1rem',
         marginTop: '1rem', 
         marginBottom: '1rem',
         textAlign: 'center',
         fontSize: '2rem',
         textTransform: 'uppercase',
-        color: '#85BAA1',
-        // display: 'flex', 
-        // flexDirection: 'colummn'
+        color: '#E4FDE1',
     },
     section: {
         display: 'flex',
-        // flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         padding: '0rem',
@@ -59,12 +69,20 @@ const styles = {
         padding: '.5rem',
         marginLeft: '1rem',
         borderRadius: '10px',
-        backgroundColor: '#85BAA1',
+        backgroundColor: '#E4FDE1 ',
         border: 'none',
+        width: '5rem', 
+        height: '2.1rem', 
+        fontWeight: '600', 
+        textTransform: 'uppercase'
         // color: 'white'
     }, 
+    header: { 
+        letterSpacing: '.06rem'
+    },
     instructions: { 
-        fontSize: '1.5 rem'
+        fontSize: '1.5rem', 
+        letterSpacing: '.04rem'
     }
 
 
